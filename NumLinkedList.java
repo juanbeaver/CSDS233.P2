38ae55ffd5463f818d9e0fe4fc6be5475fb36841
Juan Beaver <jcarlosbeaver@live.com>
2022-02-06 15:26:59 -0500

    Wrote the NumList interface, and wrote the skeleton for the NumLinkedList class.

---
+++ NumLinkedList.java
@@ -0,0 +1,45 @@
+public class NumLinkedList implements NumList{
+    public NumLinkedList(){
+
+    }
+
+    public int size(){
+        return 0;
+    }
+
+    public int capacity(){
+        return 0;
+    }
+
+    public void add(double value){
+
+    }
+
+    public void insert(int i, double value){
+
+    }
+
+    public void remove(int i){
+
+    }
+
+    public boolean contains(double value){
+        return false;
+    }
+
+    public double lookup(int i){
+        return 0;
+    }
+
+    public boolean equals(NumList otherList){
+        return false;
+    }
+
+    public void removeDuplicates(){
+
+    }
+
+    public String toString(){
+        return null;
+    }
+}
